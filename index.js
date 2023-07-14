c25cab0cda626ef382738b75af42571dfd27d114
GustavaoMoura <139025213+GustavaoMoura@users.noreply.github.com>
2023-07-13 23:53:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const personagens =  document.querySelectorAll('.personagem');
+
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450){
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+
+        removerSelecaoDePersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alteraraImagemPersonageSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
+
+    })
+})
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+
+    nomePersonagem.innerText = personagem.getAttribute('data-nome');
+}
+
+function alteraraImagemPersonageSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+    const idPersonagem = personagem.attributes.id.value;
+    console.log(idPersonagem);
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDePersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
